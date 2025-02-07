cb88bcfedc604cb1de6b8f1ce24573ccfde8c0cf
codesthenos <codesthenos@gmail.com>
2025-02-07 13:01:24 +0100

    primera version de la tweetspage que simplemente hace fetch para traer los tweets y si no hay muestra un mensaje y si hay los muestra, o deberia, ahroa comprobare cuando cree la primera version de crear un advert

--- src/pages/adverts/Adverts.tsx
+++ src/pages/adverts/Adverts.tsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from 'react'
+import { Advert } from './types'
+import { getAdverts } from './service'
+
 export const AdvertsPage = () => {
+  const [adverts, setAdverts] = useState<Advert[]>([])
+
+  useEffect(() => {
+    const fetchAdverts = async () => {
+      const fetchedAdverts = await getAdverts()
+      setAdverts(fetchedAdverts)
+    }
+    fetchAdverts()
+  }, [])
   return (
-    <h1 className="text-center text-6xl font-bold uppercase">Adverts Page</h1>
+    <>
+      <h1 className="text-center text-6xl font-bold uppercase">
+        TODO Adverts Page
+      </h1>
+      <ul>
+        {adverts.length ? (
+          adverts.map((advert) => (
+            <li key={advert.id}>
+              <h3>{advert.name}</h3>
+              <h3>{advert.price}</h3>
+            </li>
+          ))
+        ) : (
+          <h2 className="mt-8 text-center text-4xl font-bold text-amber-400">
+            NO TWEETS
+          </h2>
+        )}
+      </ul>
+    </>
   )
 }
