e243339ae8db126ff425adf716dc71e9a687886b
codesthenos <codesthenos@gmail.com>
2025-02-08 22:49:51 +0100

    migrado el estado de los filtros a searchParams, todo funciona igual de bien que antes, pero ahora persisto el estado en la url

--- src/components/adverts/FilterForm.tsx
+++ src/components/adverts/FilterForm.tsx
@@ -1,7 +1,8 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { FilterInputText } from './FilterInputText'
 import { FilterButton } from './FilterButton'
 import { Checkbox } from '../Checkbox'
+import { useSearchParams } from 'react-router-dom'
 
 interface Filters {
   name: string | null
@@ -9,28 +10,44 @@ interface Filters {
 }
 
 export const FilterForm = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
   const [filters, setFilters] = useState<Filters>({ name: null, sale: null })
 
   const nameRef = useRef<HTMLInputElement>(null)
   const saleRef = useRef<HTMLInputElement>(null)
 
+  useEffect(() => {
+    const name = searchParams.get('name')
+    const sale = searchParams.get('sale')
+    const booleanSale = sale === 'true'
+
+    setFilters({ name, sale: booleanSale })
+
+    if (nameRef.current) {
+      nameRef.current.value = name || ''
+    }
+    if (saleRef.current) {
+      saleRef.current.checked = booleanSale
+    }
+  }, [searchParams])
+
   const handleApplyFilters = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
-    setFilters({ name: nameRef.current!.value, sale: saleRef.current!.checked })
+    const saleSearchParam = saleRef.current!.checked ? 'true' : 'false'
+
+    setSearchParams({ name: nameRef.current!.value, sale: saleSearchParam })
   }
 
   const handleResetFilter = () => {
-    setFilters({ name: null, sale: null })
-    nameRef.current!.value = ''
-    saleRef.current!.checked = false
+    setSearchParams()
   }
   console.log(filters)
   return (
     <form onSubmit={handleApplyFilters} className="mx-auto mt-4 max-w-xs">
       <FilterInputText inputId="nameFilter" labelText="name" ref={nameRef} />
       <Checkbox labelFor="saleFilter" labelText="ON SALE" ref={saleRef} />
-      <div className="mt-4 flex items-center justify-between">
+      <div className="mt-4 flex items-center justify-around">
         <FilterButton
           type="button"
           buttonText="RESET"
