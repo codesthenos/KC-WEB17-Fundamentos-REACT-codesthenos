d4c41a2a9d6ca8f0fafe8c3f382b1a5237dcbb0d
codesthenos <codesthenos@gmail.com>
2025-02-08 05:38:42 +0100

    manejo estados de error y loading en la lista de adverts, aunque no muestro lo que necesito mostrar ni el en happy path ni en el caso de no tener adverts

--- src/components/adverts/AdvertsList.tsx
+++ src/components/adverts/AdvertsList.tsx
@@ -3,29 +3,52 @@ import { getAdverts } from '../../pages/adverts/service'
 import type { Advert } from '../../pages/adverts/types'
 import { NoAdverts } from './NoAdverts'
 import { AdvertItem } from './AdvertItem'
+import { LoadingSpinner } from '../LoadingSpinner'
+import type { ApiClientError } from '../../api/error'
+import { isApiClientError } from '../../api/client'
 
 export const AdvertsList = () => {
   const [adverts, setAdverts] = useState<Advert[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState<ApiClientError | null>(null)
 
   useEffect(() => {
     const fetchAdverts = async () => {
-      const fetchedAdverts = await getAdverts()
-      setAdverts(fetchedAdverts)
+      try {
+        const fetchedAdverts = await getAdverts()
+        setAdverts(fetchedAdverts)
+      } catch (error) {
+        if (isApiClientError(error)) {
+          setError(error)
+        }
+      } finally {
+        setIsLoading(false)
+      }
     }
     fetchAdverts()
   }, [])
 
+  const showList = !isLoading && !error && !!adverts.length
+  const showNoAdverts = !isLoading && !error && !adverts.length
+
   return (
     <>
-      {adverts.length ? (
+      {error && (
+        <span className="mb-3 block text-red-500">{error.message}</span>
+      )}
+      {isLoading && (
+        <div className="absolute inset-0 flex items-center justify-center rounded-2xl bg-zinc-900">
+          <LoadingSpinner />
+        </div>
+      )}
+      {showList && (
         <ul>
           {adverts.map((advert) => (
             <AdvertItem advert={advert} key={advert.id} />
           ))}
         </ul>
-      ) : (
-        <NoAdverts />
       )}
+      {showNoAdverts && <NoAdverts />}
     </>
   )
 }
