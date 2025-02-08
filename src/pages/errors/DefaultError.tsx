372b76f06cada24ff618464fdc6afb35348b620c
codesthenos <codesthenos@gmail.com>
2025-02-08 08:40:19 +0100

    incluyo el link a mi portfolio en el errorboundary y simplifico la componetizacion que habia hecho del anchor de home, no le veia mucho sentido a componetizar una pagina que todo lo que usa es para ella y es simple en principio, pa que meter mas archivos

--- src/pages/errors/DefaultError.tsx
+++ src/pages/errors/DefaultError.tsx
@@ -1,12 +1,32 @@
-import { ErrorHomeAnchor } from '../../components/errors/ErrorHomeAnchor'
-
 export const DefaultErrorPage = () => {
   return (
-    <article className="mx-auto mt-8 max-w-3xl">
+    <section className="mx-auto mt-8 max-w-3xl">
       <h2 className="mb-4 text-center text-4xl font-bold">Ooooops!</h2>
-      <p className="mb-2 text-center font-semibold">Something went wrong!</p>
-      <h3 className="mb-4 text-center font-semibold">Try going home!</h3>
-      <ErrorHomeAnchor />
-    </article>
+      <p className="mb-6 text-center font-semibold">Something went wrong!</p>
+      <div className="sm:flex sm:items-center sm:justify-center sm:gap-x-8">
+        <article>
+          <h3 className="mb-4 text-center font-semibold">
+            Visit my portfolio!
+          </h3>
+          <a
+            href="https://codesthenos.github.io/CODESTHENOS-PORTFOLIO/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="mx-auto mb-2 block w-fit rounded-2xl border-2 bg-zinc-200 px-3 py-1 font-bold text-zinc-800 uppercase transition-all duration-500 ease-in-out hover:scale-110 hover:bg-zinc-800 hover:text-zinc-200"
+          >
+            CODESTHENOS
+          </a>
+        </article>
+        <article>
+          <h3 className="mb-4 text-center font-semibold">Try going home!</h3>
+          <a
+            href="/"
+            className="mx-auto mb-2 block w-fit rounded-2xl border-2 bg-zinc-800 px-3 py-1 font-bold text-zinc-200 uppercase transition-all duration-500 ease-in-out hover:scale-110 hover:bg-zinc-200 hover:text-zinc-800"
+          >
+            HOME
+          </a>
+        </article>
+      </div>
+    </section>
   )
 }
