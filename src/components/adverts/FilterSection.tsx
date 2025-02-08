bc33c4178e8c65d3fb3dac39414e6162bdc582fd
codesthenos <codesthenos@gmail.com>
2025-02-08 20:29:44 +0100

    empiezo con los filtros

---
+++ src/components/adverts/FilterSection.tsx
@@ -0,0 +1,11 @@
+export const FilterSection = () => {
+  return (
+    <section>
+      <header>
+        <h2 className="mt-8 text-center text-2xl font-semibold">
+          FILTERS SECTION
+        </h2>
+      </header>
+    </section>
+  )
+}
--- src/pages/adverts/Adverts.tsx
+++ src/pages/adverts/Adverts.tsx
@@ -1,11 +1,13 @@
 import { Header } from '../../components/adverts/Header'
 import { Footer } from '../../components/Footer'
 import { AdvertsList } from '../../components/adverts/AdvertsList'
+import { FilterSection } from '../../components/adverts/FilterSection'
 
 export const AdvertsPage = () => {
   return (
     <>
       <Header pathName="Adverts" />
+      <FilterSection />
       <main className="mx-auto mt-16 mb-4 flex max-w-3xl grow flex-col items-center text-center font-semibold uppercase">
         <AdvertsList />
       </main>
