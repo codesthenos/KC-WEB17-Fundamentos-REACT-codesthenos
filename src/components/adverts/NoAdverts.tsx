b7ee3a885eb8a4110ff7736e14b39db16da8d230
codesthenos <codesthenos@gmail.com>
2025-02-09 13:49:15 +0100

    cambio el mensaje si el resultado de array vacio es por que no hay matches en un filtro o porque no hay nigun anuncio

--- src/components/adverts/NoAdverts.tsx
+++ src/components/adverts/NoAdverts.tsx
@@ -1,11 +1,15 @@
+import { useFilters } from '../../contexts/filters/filtersContext'
 import { CreateAdvertButton } from './CreateAdvertButton'
 
 export const NoAdverts = () => {
+  const { filters } = useFilters()
+  const isAnyFilterActive = filters.name || filters.sale || filters.demand
+  const text = isAnyFilterActive
+    ? 'Any advert matches the filters'
+    : 'Create the first advert!'
   return (
     <div className="flex flex-col items-center justify-center">
-      <h3 className="font-semi my-8 text-center text-2xl">
-        Create the first advert!
-      </h3>
+      <h3 className="font-semi my-8 text-center text-2xl">{text}</h3>
       <CreateAdvertButton />
     </div>
   )
